174f37e180ab5a71da45122f41507917eeb8ca7f
Benedikt Ehinger <behinger@uos.de>
2017-03-05 15:35:21 +0100

    added new defaultcolors for eeglab

--- be_reref.m
+++ be_reref.m
@@ -3,7 +3,12 @@ function EEG = be_reref(EEG,p,sub)
     if strcmp(p.data(sub).eeg(end-2:end),'cnt')
         warning('ANT System detected: Removed additionally FCz to account for the rank reduction')
         % remove a 'random' channel due to rankreduction with av-ref
-        EEG = pop_select(EEG,'nochannel',find(cellfun(@(x)~isempty(x),regexpi({EEG.chanlocs.labels},'^FCz'))));
+        remChan = find(cellfun(@(x)~isempty(x),regexpi({EEG.chanlocs.labels},'^FCz')));
+        if isempty(remChan)
+            warning('could not find FCz, removing FT10 instead')
+            remChan = find(cellfun(@(x)~isempty(x),regexpi({EEG.chanlocs.labels},'FT10')));
+        end
+        EEG = pop_select(EEG,'nochannel',remChan);
     end
     EEG.preprocess = [EEG.preprocess '_refav'];
     EEG.preprocessInfo.reference = 'AVG';
---
+++ icadefs.m
@@ -0,0 +1,6 @@
+% function icadefs
+
+
+run([fileparts(which('eegrej')) filesep 'icadefs.m']);
+DEFAULT_COLORMAP = 'cmap_rdylbu';
+% end
